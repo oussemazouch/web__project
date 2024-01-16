db4f65f7f5270ba3a6ad205f681ae6ab599445cd
Iheb Abdelkader <iheb30abdelkader@gmail.com>
2024-01-16 22:28:39 +0100

    configured TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Common\commonModule } from './common/common/common/common.module';
+
 import { CommonModule } from './common/common.module';
 import { ConfigModule } from './config/config.module';
 import { ModelsModule } from './models/models.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/user/entities/user.entity';
 
 @Module({
-  imports: [Common\commonModule, CommonModule, ConfigModule, ModelsModule],
+  imports: [CommonModule, 
+    ConfigModule,
+     ModelsModule,
+     TypeOrmModule.forRoot({
+			type: "mysql",
+			host: "localhost",
+			port: 3306,
+			username: "root",
+			password: "",
+			database: "gymDB",
+			entities: [User],
+			synchronize: true,
+			logging: true,
+		}),],
   controllers: [AppController],
   providers: [AppService],
 })
