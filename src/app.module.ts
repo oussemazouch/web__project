2d5209c5645faffab512c974b911352ba28f86c2
Iheb Abdelkader <iheb30abdelkader@gmail.com>
2024-01-16 22:04:34 +0100

    added config common and models modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Common\commonModule } from './common/common/common/common.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from './config/config.module';
+import { ModelsModule } from './models/models.module';
 
 @Module({
-  imports: [],
+  imports: [Common\commonModule, CommonModule, ConfigModule, ModelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
