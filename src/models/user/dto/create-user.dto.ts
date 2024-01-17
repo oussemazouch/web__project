ecca615e7068e3347ce659758de78b8403f34eda
Iheb Abdelkader <iheb30abdelkader@gmail.com>
2024-01-17 16:21:56 +0100

    made the User entity and the UserDTO

--- src/models/user/dto/create-user.dto.ts
+++ src/models/user/dto/create-user.dto.ts
@@ -1,3 +1,25 @@
+import { IsEmail, IsNotEmpty, IsNumber, Max, Min } from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
     name:string;
+    @IsNotEmpty()
+    lastName:string;
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
+    @Max(100)
+    age:number;
+    @IsNotEmpty()
+    @IsNumber()
+    phoneNumber:number;
+    @IsEmail()
+    @IsNotEmpty()
+    email:string;
+    address:string;
+    @IsNotEmpty()
+    password:string;
+    @IsNumber()
+    @Min(0)
+    solde:number;
 }
