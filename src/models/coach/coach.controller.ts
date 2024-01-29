2a90392df38d457f64bfad3fc732b4808acfde43
aziz <aziz.merhben2001@gmail.com>
2024-01-29 23:13:41 +0100

    fixes

--- src/models/coach/coach.controller.ts
+++ src/models/coach/coach.controller.ts
@@ -6,16 +6,29 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { CoachService } from './coach.service';
 import { CreateCoachDto } from './dto/create-coach.dto';
 import { UpdateCoachDto } from './dto/update-coach.dto';
 import { Coach } from './entities/coach.entity';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Page } from 'src/common/dtos/page.dto';
 
 @Controller('coach')
 export class CoachController {
   constructor(private readonly coachService: CoachService) {}
 
+
+  @Get('list')
+  async getProductsByPage(@Query() pageData: Page): Promise<Pagination<Coach>>
+  {
+    return this.coachService.paginate({
+      page:pageData.page,
+      limit:pageData.limit,
+
+    });
+  }
   @Post()
   async create(@Body() createCoachDto: CreateCoachDto) {
     return await this.coachService.create(createCoachDto);
